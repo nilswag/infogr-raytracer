cedc48acf590181f74f713360f0a26979cad5ae8
nilswag <nilswagemaker05@icloud.com>
2026-05-16 17:03:31 +0200

    Added point light class

---
+++ Light.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace INFOGRTemplate
+{
+    public class Light
+    {
+        // Position of point light
+        public Vector3 Pos { get; set; }
+
+        // Intensity of point light (I = (r, g, b))
+        public Vector3 Intensity { get; set; }
+
+        public Light(Vector3 pos, Vector3 intensity)
+        {
+            Pos = pos;
+            Intensity = intensity;
+        }
+    }
+}
